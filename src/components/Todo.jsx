72c1c9de3b94df0ccf5e5521ffd4bce42180cf18
S0ngyoungjun <clesaaaaaa@gmail.com>
2024-08-24 22:35:00 +0900

    띄어쓰기

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -24,12 +24,12 @@ const Todo = () => {
     setSearchTerm(value);
     dispatch(updateSearchTerm(value)); // Redux 상태에 searchTerm 업데이트
   };
-  
+
   return (
     <div className="max-w-2xl mx-auto sm:mt-8 p-4 bg-slate-200 rounded">
       <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">할일 관리</h2>
       <div className="flex items-center mb-4">
-         <input value={newTodoText} onChange= {(e) => setNewTodoText(e.target.value)} type="text" name="addTodoInput" id="addTodoInput" placeholder="할일 추가"
+         <input value={newTodoText} onChange= {(e) => setNewTodoText(e.target.value)} type="text" name="addTodoInput" id="addTodoInput" placeholder="할 일 추가"
          className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-gray-800"/>
          <button className="ml-4 p-2 bg-blue-300 text-white rounded hover:bg-blue-950
          focus:outline-none" onClick={handleAddToDoClick}><BsPlus/></button> 
@@ -39,7 +39,7 @@ const Todo = () => {
           <FilterButton/>
          <div className="flex items-center mb-4">
           <input value={searchTerm} onChange={(e) => handleSearchChange(e.target.value)} type="text"
-          name="searchTodoInput" id="searchTodoInput" placeholder="할일 검색" className="flex-grow p-2 border-b-2
+          name="searchTodoInput" id="searchTodoInput" placeholder="할 일 검색" className="flex-grow p-2 border-b-2
           border-graay-300 focus:outline-none focus:border-blue-500"></input>
          </div>
          <button className="ml-4 p-2 bg-blue-300 text-white rounded hover:bg-blue-950
